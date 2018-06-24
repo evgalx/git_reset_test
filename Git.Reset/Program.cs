849150bb2c8d1cb17b99790d090bd8001975aef0
my.home <alx.evg@gmail.com>
2018-06-24 20:09:07 +0300

    02_test branch

--- Git.Reset/Program.cs
+++ Git.Reset/Program.cs
@@ -7,6 +7,7 @@ namespace Git.Reset
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World 02_test!");
         }
     }
 }
