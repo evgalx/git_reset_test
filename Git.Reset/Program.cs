197ab6fecb8082fff20b7414a8f1ee418788d0cf
my.home <alx.evg@gmail.com>
2018-06-24 20:13:16 +0300

    03_test branch added

--- Git.Reset/Program.cs
+++ Git.Reset/Program.cs
@@ -6,6 +6,7 @@ namespace Git.Reset
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World 03_test added");
             Console.WriteLine("Hello World Branch 01_test!");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World 02_test!");
